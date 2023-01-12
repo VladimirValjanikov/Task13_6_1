471461fc7dbbb719f9c6cc43162662b4875bad3a
Vladimir Valjanikov <valjanikov-vladimir@yandex.ru>
2023-01-12 11:51:12 +0300

    Добавьте файлы проекта.

---
+++ Task13_6_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace Task13_6_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var array = new int[1000000];
+
+			var list = new List<int>(array);
+			var linkedList = new LinkedList<int>(array);
+
+			var watch = Stopwatch.StartNew();
+			list.Insert(1, 1);
+			Console.WriteLine($"Вставка в list: {watch.Elapsed.TotalMilliseconds}  мс");
+			
+			watch.Restart();
+
+			linkedList.AddAfter(linkedList.First, 1);
+			Console.WriteLine($"Вставка в linkedList: {watch.Elapsed.TotalMilliseconds}  мс");
+			watch.Stop();
+
+			Console.ReadLine();
+		}
+	}
+}
